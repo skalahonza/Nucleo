69be56dc15c3b95761fb26b51bb45b2a0e34285b
Jan Skala <j.skala@hotmail.com>
2016-11-25 19:43:56 +0100

    Create basic layout for the code.

--- main.c
+++ main.c
@@ -4,7 +4,63 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <termios.h>
+
+#define THREADS_COUNT 2
+
+int hSerial;
+struct termios o_tty;
+bool quit = false;
+pthread_mutex_t mtx;
+
+void call_stty(int reset)
+{
+    if (reset)
+    {
+        reset = system("stty -raw");
+    }
+    else
+    {
+        reset = system("stty raw");
+    }
+}
+
+void *thread1(void *v)
+{
+    bool q = false;
+    while (!q)
+    {
+        //Read user input
+        q = quit;
+    }
+    return 0;
+}
+
+void *thread2(void *v)
+{
+    bool q = false;
+    while (!q)
+    {
+        //read serial data
+        char chArrBuf[256];
+        memset(&chArrBuf, '\0', sizeof(chArrBuf));
+        int n = read(hSerial, &chArrBuf, sizeof(chArrBuf));
+        if (n == -1)
+            {/*fprintf(stderr, "Error while reding.\n");*/}
+        else if (n == 0)
+            {/*printf("End of file reached.\n");*/}
+        else
+        {
+            printf("\r%s", chArrBuf);
+        }
+        usleep(100 * 1000);
+        q = quit;
+    }
+    return 0;
+}
 
 void print_menu()
 {
@@ -16,12 +72,6 @@ void print_menu()
     printf("Item c: Enter a custom command\n");
     printf("Item e: Exit\n");
     printf("Selection:\n");
-}
-
-int read_serial(int hSerial, char chArrBuf[256])
-{
-    memset(&chArrBuf, '\0', sizeof(chArrBuf));
-    return read(hSerial, &chArrBuf, sizeof(chArrBuf));
 }
 
 void handle_custom_command(char custom[100])
@@ -38,11 +88,11 @@ int main(int argc, char **args)
     }
     else
     {
-        //get serial
         printf("Welcome! The input parameter is %s \n", args[1]);
     }
 
-    int hSerial = open(args[1], O_RDWR | O_NONBLOCK | O_NDELAY);
+    //open serial
+    hSerial = open(args[1], O_RDWR | O_NONBLOCK | O_NDELAY);
     if (hSerial < 0)
     {
         fprintf(stderr, "Cannot open serial.\n");
@@ -51,12 +101,29 @@ int main(int argc, char **args)
         return 100;
     }
 
-    // h = handle
-    struct termios o_tty;
-    // o = object
-    memset(&o_tty, 0, sizeof(o_tty));
-    /*int iRetVal = */ tcgetattr(hSerial, &o_tty);
     print_menu();
+    call_stty(0);
+
+    memset(&o_tty, 0, sizeof(o_tty));
+    tcgetattr(hSerial, &o_tty);
+
+    //create threads
+    pthread_mutex_init(&mtx, NULL);
+    pthread_t thrs[THREADS_COUNT];
+
+    pthread_create(&thrs[0], NULL, thread1, NULL);
+    pthread_create(&thrs[1], NULL, thread2, NULL);
+
+    for (int i = 0; i < THREADS_COUNT; ++i)
+    {
+        pthread_join(thrs[i], NULL);
+    }
+
+    call_stty(1);
+    close(hSerial);
+    printf("\n");
+    return 0;
+
     while (1)
     {
 
@@ -112,7 +179,4 @@ int main(int argc, char **args)
             }
         }
     }
-
-    close(hSerial);
-    return 0;
 }
