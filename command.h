02c78942a3d7c034545b28579ee043fa0187809f
Jan Skála <j.skala@hotmail.com>
2017-01-04 11:49:08 +0100

    edit command model, add boolean

--- command.h
+++ command.h
@@ -6,32 +6,52 @@
 #define LED_COMMAND "LED\r\n"
 #define LED_COMMAND_LEN 6
 
+#define LED_ON_COMMAND "LED ON\r\n"
+#define LED_ON_COMMAND_LEN 9
+
+#define LED_OFF_COMMAND "LED OFF\r\n"
+#define LED_OFF_COMMAND_LEN 10
+
 #define BTN_COMMAND "BTN\r\n"
 #define BTN_COMMAND_LEN 6
 
-// Command types
-// SIMPLE - IDN, LED - will be send directely no response expeted
-// JOYSTICK - sends joystick request - waits for response
-typedef enum { SIMPLE,
-               JOYSTICK,
-               BUTTON } CMD_TYPE;
+#include <stdbool.h>
 
-//list of labels
+// Command types
+// IDN, LED - will be send directely no response expeted
+// JOYSTICK - sends joystick request - waits for response
+typedef enum { LED,
+               IDN,
+               JOYSTICK,
+               BUTTON,
+               DRAW_SETTEXTCOLOR,
+               DRAW_CLEAR,
+               DRAW_PIXEL,
+               DRAW_LINE,
+               DRAW_CIRCLE,
+               DRAW_SETFONT,
+               DRAW_TEXT } CMD_TYPE;
+
+// list of labels
 typedef struct t_label
 {
     struct t_label *next;
     char *id;
     int lenth;   //length of the identifier
     int jumpIdx; //index of the list that the label should jump to
-} Labels;
+} Labels_list;
 
-//list of commands
+// list of commands
 typedef struct t_command
 {
     struct t_command *next;
     char *string;  //command string
     int lenght;    //length of the command
     CMD_TYPE type; //type of the command
-} Commands;
+    bool response+ //if the command awaits response
+} Commands_list;
+
+// the nucelo board will validate the commands
+
 
 #endif /* COMMAND_H */
